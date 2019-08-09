833c4f0b85a5bf3c6bb6ce8bc856921f7e72631e
lxy009 <luxueyi@gmail.com>
2019-08-09 16:10:50 -0500

    clean_space.r POC

---
+++ clean_space.r
@@ -0,0 +1,14 @@
+
+
+
+# 
+# can create subprocess using system2().
+# call Rscript
+# need to make sure on Windows Rscript is on path file
+#
+
+# alternative is to clear workspace
+# removal all works
+# detach, i start getting problems when looping
+# some async issue??
+
